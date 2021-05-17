7669d48430221610466fd0990e18b29647740511
ZMartis <zachpmartis@gmail.com>
2021-05-16 20:13:14 -0400

    set up heading

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -14,9 +14,9 @@
 	export const prerender = true;
 </script>
 
-<svelte:head>
+<!-- <svelte:head>
 	<title>About</title>
-</svelte:head>
+</svelte:head> -->
 
 <div class="content">
 	<h1>About this app</h1>
