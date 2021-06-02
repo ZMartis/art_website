ce1435d332711b8b80063b6bbeec459444c921ae
ZMartis <zachpmartis@gmail.com>
2021-06-02 08:42:21 -0400

    made site static

--- src/lib/data/macros.ts
+++ src/lib/data/macros.ts
@@ -1,2 +1,230 @@
 import type { Artwork } from 'src/types/artwork';
-export const macros: Artwork[] = [];
+export const macros: Artwork[] = [
+	{
+		id: '0',
+		title: 'No. 1',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No1.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '1',
+		title: 'Blau',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Blau.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '2',
+		title: 'Specter',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Specter.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '3',
+		title: 'Do',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Do.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '4',
+		title: 'Obscuring Brush',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Obscuring+Brush.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '5',
+		title: 'Enlightenment',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Enlightenment.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '6',
+		title: 'WL',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/WL.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '7',
+		title: 'Creep',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Creep.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '8',
+		title: 'Dolly Zoom',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Dolly+Zoom.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '9',
+		title: 'Refraction',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Refraction.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '10',
+		title: 'Winds',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Winds.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '11',
+		title: 'Crashing Wave',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Crashing+Wave.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '12',
+		title: 'Sai',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Sai.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '13',
+		title: 'Kiss',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Kiss.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '14',
+		title: 'Pressure Waves',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Pressure+Waves.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '15',
+		title: 'de Lorme',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/de+Lorme.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '16',
+		title: 'Newtonian',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/Newtonian.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '17',
+		title: 'No19',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No19.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '18',
+		title: 'No20',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No20.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '19',
+		title: 'No21',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No21.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '20',
+		title: 'No22',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No22.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '21',
+		title: 'No23',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No23.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '22',
+		title: 'No24',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No24.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	},
+	{
+		id: '23',
+		title: 'No25',
+		url: 'https://zach-martis-images.s3.us-east-2.amazonaws.com/Macro/No25.jpg',
+		medium: 'macro',
+		price: '$100',
+		description: '',
+		sold: false
+	}
+];
--- src/lib/data/paint_on_papers.ts
+++ src/lib/data/paint_on_papers.ts
@@ -1,2 +1,13 @@
 import type { Artwork } from 'src/types/artwork';
-export const paintOnPapers: Artwork[] = [];
+export const paintOnPapers: Artwork[] = [
+	{
+		id: '1',
+		title: 'March Hare',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/March+Hare.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	}
+];
