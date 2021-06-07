2fba8c4d13452e582f0fc0d78ca670cb54ba1fc6
ZMartis <zachpmartis@gmail.com>
2021-06-06 23:26:28 -0400

    started adding in stripes

--- src/lib/data/stripes.ts
+++ src/lib/data/stripes.ts
@@ -1,2 +1,13 @@
 import type { Artwork } from 'src/types/artwork';
-export const stripes: Artwork[] = [];
+export const stripes: Artwork[] = [
+	{
+		id: '34',
+		title: 'Ace',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Stripes/Ace.webp',
+		medium: 'stripes',
+		price: '$1200',
+		description: '',
+		sold: false
+	}
+];
--- src/routes/work/pixel_sort.svelte
+++ src/routes/work/pixel_sort.svelte
@@ -4,13 +4,3 @@
 </script>
 
 <ArtDisplay artworks={pixelSorts} />
-
-<style>
-	/* your mobile styles go here */
-	@media (min-width: 768px) {
-		/* your tablet styles go here */
-	}
-	@media (min-width: 1366px) {
-		/* your desktop styles go here */
-	}
-</style>
