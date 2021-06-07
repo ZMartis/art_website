f56654cd35148866509e802ddec2226d8b26f341
ZMartis <zachpmartis@gmail.com>
2021-06-06 22:39:12 -0400

    added all paint on paper objects

--- src/lib/data/paint_on_papers.ts
+++ src/lib/data/paint_on_papers.ts
@@ -1,10 +1,350 @@
 import type { Artwork } from 'src/types/artwork';
 export const paintOnPapers: Artwork[] = [
 	{
-		id: '1',
+		id: '0',
 		title: 'March Hare',
 		url:
 			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/March+Hare.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '1',
+		title: 'Elements',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Elements.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '2',
+		title: 'Splash',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Splash.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '3',
+		title: 'Obscured',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Obscured.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '4',
+		title: 'Basque',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Basque.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '5',
+		title: 'Sandhill',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Sandhill.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '6',
+		title: 'Tea',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Tea.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '7',
+		title: 'Calamity',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Calamity.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: true
+	},
+	{
+		id: '8',
+		title: 'Feed',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Feed.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '9',
+		title: 'Flux',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Flux.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '10',
+		title: 'Foliage',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Foliage.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '11',
+		title: 'Volcanic Lake',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Volcanic+Lake.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '12',
+		title: "Day's End",
+		url:
+			"https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Day's+End.webp",
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '13',
+		title: 'Wave',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Wave.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: true
+	},
+	{
+		id: '14',
+		title: 'Slipstream',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Slipstream.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '15',
+		title: 'Consume',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Consume.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '16',
+		title: 'Security',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Security.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '17',
+		title: 'Whisper',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Whisper.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '18',
+		title: 'Companion',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Companion.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '19',
+		title: 'Airavata',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Airavata.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '20',
+		title: 'Voyager',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Voyager.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '21',
+		title: 'Emerge',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Emerge.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '22',
+		title: 'Angler',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Angler.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '23',
+		title: 'Birth of Fire',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Birth+of+Fire.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '24',
+		title: 'Self-Portrait',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Self-Portrait.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '25',
+		title: 'Lightning Bug',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Lightning+Bug.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '26',
+		title: 'Trust',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Trust.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '27',
+		title: 'Banshee',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Banshee.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '28',
+		title: 'Dragonfly',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Dragonfly.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '29',
+		title: 'Steps',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Steps.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '30',
+		title: 'Atlas',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Atlas.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '31',
+		title: 'Entangled',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Entangled.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '32',
+		title: 'Birth',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Birth.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '33',
+		title: 'Falling Pond',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Falling+Pond.webp',
+		medium: 'paintOnPaper',
+		price: '$200',
+		description: '',
+		sold: false
+	},
+	{
+		id: '34',
+		title: 'Quench',
+		url:
+			'https://zach-martis-images.s3.us-east-2.amazonaws.com/Acrylic+on+Paper/Quench.webp',
 		medium: 'paintOnPaper',
 		price: '$200',
 		description: '',
